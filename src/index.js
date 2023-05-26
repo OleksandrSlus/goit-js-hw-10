76721e44501874ed1f01a89822edda9d918aa7e2
Oleksandr <121134310+OleksandrSlus@users.noreply.github.com>
2023-05-26 12:51:22 +0300

    Zaruba0.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-import './css/styles.css';
+import debounce from "lodash.debounce";
+import { fetchCountries } from "./fetchCountries";
+import { renderCountryMarkup } from "./countryMarkup";
 
 const DEBOUNCE_DELAY = 300;
+
+export const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info')
+};
+
+const debouncedFetchCountries = debounce(() => {
+  const searchValue = refs.searchBox.value.trim();
+  fetchCountries(searchValue)
+  .then(countries => {
+    renderCountryMarkup(countries);
+  })
+}, DEBOUNCE_DELAY);
+
+refs.searchBox.addEventListener('input', debouncedFetchCountries);
+
