5d54fd660a73150ef043ff0fe791b96222c3a46d
Oleksandr <121134310+OleksandrSlus@users.noreply.github.com>
2023-06-01 09:53:02 +0300

    Zaruba 0.3 fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,25 +3,11 @@ export function fetchCountries(searchValue) {
     params.set('fields', 'name,capital,languages,population,flags');
     const url = `https://restcountries.com/v3.1/name/${searchValue}?${params.toString()}`;
   
-    if (searchValue === "") {
-      return Promise.resolve([]);
-    }
-  
-    return fetch(url)
+       return fetch(url)
       .then((response) => {
         if (!response.ok) {
           throw new Error(response.status);
         }
         return response.json();
       })
-      .then((data) => {
-        if (data.status === 404) {
-          throw new Error("Country not found");
-        }
-        return data;
-      })
-      .catch((error) => {
-        console.error("Error fetching countries:", error);
-        return [];
-      });
   }
